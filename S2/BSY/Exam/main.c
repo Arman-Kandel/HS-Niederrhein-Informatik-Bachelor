d2a3265eb1e12c0249d41351540e72e8f67c6d26
Osiris Team <osiris_support@pm.me>
2022-06-21 14:26:43 +0200

    bsy

--- S2/BSY/Exam/main.c
+++ S2/BSY/Exam/main.c
@@ -4,16 +4,6 @@
 #include <string.h>
 #include <errno.h>
 #include "my-utils.h"
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0')
 #ifndef _WIN32
 #include "wiringPi.h"
 #endif
