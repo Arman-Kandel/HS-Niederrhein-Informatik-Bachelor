222557b95939bb7a03fc734c397e9e3585ea8113
Arman-Kandel <91139101+Arman-Kandel@users.noreply.github.com>
2022-11-06 21:27:27 +0100

    Create civicrm-installer.sh

---
+++ S3/WEB/civicrm-installer.sh
@@ -0,0 +1,40 @@
+#/bin/sh
+# Get CiviCRM
+wget https://download.civicrm.org/civicrm-5.55.0-wordpress.zip
+unzip civicrm-5.55.0-wordpress.zip -d /var/www/civicrm/wp-content/plugins/
+
+# Configure Apache
+cat << EOF >> /etc/apache2/sites-available/civicrm.conf
+<VirtualHost *:80>
+
+    ServerAdmin admin@example.com
+
+    ServerName example.com
+
+    DocumentRoot /var/www/civicrm
+
+
+
+    <Directory /var/www/civicrm>
+
+        Options Indexes FollowSymLinks
+
+        AllowOverride All
+
+        Require all granted
+
+    </Directory>
+
+
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+</VirtualHost>
+EOF
+
+a2ensite /etc/apache2/sites-available/civicrm.conf
+a2dissite /etc/apache2/sites-available/000-default.conf
+a2enmod rewrite
+systemctl restart apache2
