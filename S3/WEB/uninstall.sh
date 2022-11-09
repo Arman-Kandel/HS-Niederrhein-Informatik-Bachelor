524a75c920768f876d74e7dcf9dadf6c99f82467
Arman-Kandel <91139101+Arman-Kandel@users.noreply.github.com>
2022-11-09 17:33:10 +0100

    Update uninstall.sh

--- S3/WEB/uninstall.sh
+++ S3/WEB/uninstall.sh
@@ -1,6 +1,12 @@
 #/bin/sh
 # Note that this script must be ran as root user, thus do this before: sudo su -
 echo "==="
+echo "Deleting wordpress database and user..."
+echo "==="
+/usr/bin/mariadb -u root -e "DROP DATABASE wordpress"
+/usr/bin/mariadb -u root -e "DROP USER wordpress"
+
+echo "==="
 echo "Removing dependencies..."
 echo "==="
 dpkg --configure -a
@@ -10,10 +16,12 @@ apt -y purge mariadb-server
 apt -y purge mariadb-client
 apt -y purge php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml
 apt autoremove # Removes not needed dependencies
+
 echo "==="
 echo "Uninstalling WordPress..."
 echo "==="
 rm -r /var/www/html
+
 echo "==="
 echo "Finished!"
 echo "==="
