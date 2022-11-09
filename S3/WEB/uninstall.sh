4dd0e9db574470c0e55e904de3edb8c5ee2ffd14
Arman-Kandel <91139101+Arman-Kandel@users.noreply.github.com>
2022-11-09 18:11:39 +0100

    Update uninstall.sh

--- S3/WEB/uninstall.sh
+++ S3/WEB/uninstall.sh
@@ -14,7 +14,7 @@ apt -y purge lynx
 apt -y purge apache2
 apt -y purge mariadb-server 
 apt -y purge mariadb-client
-apt -y purge php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml
+apt -y purge php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml php-zip
 apt autoremove # Removes not needed dependencies
 
 echo "==="
