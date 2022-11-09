d87aa4bb45a6905889187a5b6e93113ef0491714
Arman-Kandel <91139101+Arman-Kandel@users.noreply.github.com>
2022-11-09 15:27:51 +0100

    Update and rename wordpress-installer.sh to install.sh

--- S3/WEB/install.sh
+++ S3/WEB/install.sh
@@ -1,9 +1,12 @@
 #/bin/sh
 # Note that this script must be ran as root user, thus do this before: sudo su -
 # Source: https://www.how2shout.com/linux/script-to-install-lamp-wordpress-on-ubuntu-20-04-lts-server-quickly-with-one-command/
+# Source: https://www.linuxcapable.com/how-to-install-wordpress-with-lamp-stack-on-debian-11-bullseye/
 
+echo "==="
+echo "Creating credentials..."
+echo "==="
 install_dir="/var/www/html"
-#Creating Random WP Database Credenitals
 db_name="wp`date +%s`"
 db_user=$db_name
 db_password=`date |md5sum |cut -c '1-12'`
@@ -11,11 +14,16 @@ sleep 1
 mysqlrootpass=`date |md5sum |cut -c '1-12'`
 sleep 1
 
-#### Install Packages for https and mysql
+echo "==="
+echo "Installing and enabling dependencies..."
+echo "==="
 apt -y update 
 apt -y upgrade
+apt -y install zip
+apt -y install lynx
 apt -y install apache2
-apt -y install mysql-server
+curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash -s -- --mariadb-server-version=10.7 --skip-maxscale --skip-tools
+apt -y install mariadb-server mariadb-client
 
 
 #### Start http
@@ -23,19 +31,20 @@ rm /var/www/html/index.html
 systemctl enable apache2
 systemctl start apache2
 
-#### Start mysql and set root password
+#### Start mariadb and set root password
 
-systemctl enable mysql
-systemctl start mysql
+systemctl enable mariadb
+systemctl start mariadb
 
-/usr/bin/mysql -e "USE mysql;"
-/usr/bin/mysql -e "UPDATE user SET Password=PASSWORD($mysqlrootpass) WHERE user='root';"
-/usr/bin/mysql -e "FLUSH PRIVILEGES;"
+/usr/bin/mariadb -e "USE mysql;"
+/usr/bin/mariadb -e "UPDATE user SET Password=PASSWORD($mysqlrootpass) WHERE user='root';"
+/usr/bin/mariadb -e "FLUSH PRIVILEGES;"
 touch /root/.my.cnf
 chmod 640 /root/.my.cnf
 echo "[client]">>/root/.my.cnf
 echo "user=root">>/root/.my.cnf
 echo "password="$mysqlrootpass>>/root/.my.cnf
+
 ####Install PHP
 apt -y install php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml
 
@@ -46,12 +55,19 @@ systemctl restart apache2
 ####Download and extract latest WordPress Package
 if test -f /tmp/latest.tar.gz
 then
-echo "WP is already downloaded."
+echo "==="
+echo "WordPress is already downloaded."
+echo "==="
 else
-echo "Downloading WordPress"
+echo "==="
+echo "Downloading WordPress..."
+echo "==="
 cd /tmp/ && wget "http://wordpress.org/latest.tar.gz";
 fi
 
+echo "==="
+echo "Installing WordPress..."
+echo "==="
 /bin/tar -C $install_dir -zxf /tmp/latest.tar.gz --strip-components=1
 chown www-data: $install_dir -R
 
@@ -86,12 +102,44 @@ grep -A50 'table_prefix' $install_dir/wp-config.php > /tmp/wp-tmp-config
 /bin/sed -i '/**#@/,/$p/d' $install_dir/wp-config.php
 /usr/bin/lynx --dump -width 200 https://api.wordpress.org/secret-key/1.1/salt/ >> $install_dir/wp-config.php
 /bin/cat /tmp/wp-tmp-config >> $install_dir/wp-config.php && rm /tmp/wp-tmp-config -f
-/usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
-/usr/bin/mysql -u root -e "CREATE USER '$db_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
-/usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
- 
+/usr/bin/mariadb -u root -e "CREATE DATABASE $db_name"
+/usr/bin/mariadb -u root -e "CREATE USER '$db_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
+/usr/bin/mariadb -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
+
+echo "==="
+echo "Installing CiviCRM..."
+echo "==="
+# Get CiviCRM
+wget -O civicrm.zip https://download.civicrm.org/civicrm-5.55.0-wordpress.zip
+unzip civicrm.zip -d /var/www/html/wp-content/plugins/
+
+# Configure Apache
+cat << EOF >> /etc/apache2/sites-available/civicrm.conf
+<VirtualHost *:80>
+    ServerAdmin admin@example.com
+    ServerName example.com
+    DocumentRoot /var/www/html
+    <Directory /var/www/html>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+a2ensite /etc/apache2/sites-available/civicrm.conf
+a2dissite /etc/apache2/sites-available/000-default.conf
+a2enmod rewrite
+systemctl restart apache2
+
+echo "==="
+echo "Finished! Save the generated credentials somewhere."
+echo "==="
 ######Display generated passwords to log file.
+echo "WordPress available at: http://localhost"
 echo "Database Name: " $db_name
 echo "Database User: " $db_user
 echo "Database Password: " $db_password
-echo "Mysql root password: " $mysqlrootpass
+echo "MariaDB root password: " $mysqlrootpass
