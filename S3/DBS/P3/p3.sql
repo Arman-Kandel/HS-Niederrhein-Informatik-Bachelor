fc901eff0dc20d75e4bf1828632b4ec00033f6fa
Arman-Kandel <91139101+Arman-Kandel@users.noreply.github.com>
2023-01-03 13:16:39 +0100

    Update p3.sql

--- S3/DBS/P3/p3.sql
+++ S3/DBS/P3/p3.sql
@@ -1,5 +1,9 @@
 -- psql -U dbs141 -h 194.94.121.224 -p 15434 dbs141
 -- \i ~/arman/p3.sql
+
+\echo ####
+\echo AUFGABE 1
+\echo ####
 CREATE TABLE abteilung(
     anr INT PRIMARY KEY,
     name VARCHAR(30)
@@ -19,24 +23,48 @@ CREATE TABLE gehaltshistorie(
     gehaltNeu NUMERIC(8,2),
     gueltigAbAlt DATE,
     gueltigAbNeu DATE,
+    changed DATE,
     name VARCHAR(30) -- username of database user that performed this change
 );
 
-CREATE OR REPLACE FUNCTION check_account_update()      
+CREATE OR REPLACE FUNCTION check_change()      
 LANGUAGE plpgsql  
 AS $variable_name$    
 DECLARE    
-declaration;    
-[...] -- variable declaration   
+averageGehalt NUMERIC(8,2),   
+maxGehalt NUMERIC(8,2)   
  BEGIN    
-< function_body >    
-[...]  -- logic  
-RETURN { variable_name | value }    
+-- Regel 1: Das Gehalt einer neu eingefügten oder geänderten Person darf um maximal 50%
+-- höher sein als der bisherige Durchschnitt in der Abteilung der Person. (Dies gilt
+-- nur, wenn es weitere Personen in der gleichen Abteilung gibt).
+averageGehalt = SELECT AVG(gehalt) FROM person WHERE abteilungsnr = NEW.abteilungsnr;
+maxGehalt = averageGehalt + averageGehalt / 2; -- 50%
+IF (NEW.gehalt > maxGehalt) THEN
+  RAISE EXCEPTION 'Regel 1! % > %', NEW.gehalt, maxGehalt;
+END IF;
+-- Regel 2: Das Gehalt einer Person darf nicht reduziert werden.
+IF (NEW.gehalt < OLD.gehalt) THEN
+  RAISE EXCEPTION 'Regel 2! % < %' NEW.gehalt, OLD.gehalt;
+END IF;
+-- Regel 3: Das neue Gehalt einer Person darf höchstens 120% ihres bisherigen Gehalts
+-- betragen. Sollte das neue Gehalt höher sein, wird es automatisch auf 120% des
+-- bisherigen Gehalts gekappt.
+maxGehalt = OLD.gehalt + OLD.gehalt / 5; -- 20%
+IF (NEW.gehalt > maxGehalt) THEN
+  NEW.gehalt = maxGehalt;
+END IF;
+-- Regel 4: Es dürfen keine rückwirkenden Gehaltsänderungen vorgenommen werden, d.h.
+-- das neue gueltigAb-Datum darf nicht zeitlich früher liegen als das bisherige
+-- gueltigAb-Datum.  
+IF (NEW.gueltigAb < OLD.gueltigAb) THEN
+  RAISE EXCEPTION 'Regel 4! % < %' NEW.gueltigAb, OLD.gueltigAb;
+END IF;
+INSERT INTO gehaltshistorie VALUES(NEW.pnr, OLD.gehalt, NEW.gehalt, OLD.gueltigAb, NEW.gueltigAb, current_date(), current_user());
 END;   
 CREATE OR REPLACE TRIGGER check_update
     BEFORE UPDATE OR INSERT ON person
     FOR EACH ROW
-    EXECUTE FUNCTION check_account_update();
+    EXECUTE FUNCTION check_change();
 
 
 /* 
@@ -75,6 +103,10 @@ UPDATE Person SET gehalt = 4500, gueltigAb = to_date('01.01.2021','DD.MM.YYYY') 
 UPDATE Person SET gehalt = 4200, gueltigAb = to_date('01.01.2021','DD.MM.YYYY') WHERE pnr = 3;
 -- Gehalt wird auf 4080 gekappt wegen Regel 3,
 -- OK → Update und neuer Eintrag in Gehaltshistorie
+
+\echo ####
+\echo AUFGABE 2
+\echo ####
 
 
 DROP TABLE abteilung;
